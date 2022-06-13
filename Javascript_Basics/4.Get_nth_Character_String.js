65c2c1ceb86694e85bdbfc24f319588e4400c6e5
Viraj-Mahalunkar <mahalunkar.viraj@gmail.com>
2022-06-13 09:23:22 +0530

    moved the file from erroneously placing in parent repo to sub folder

---
+++ Javascript_Basics/4.Get_nth_Character_String.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+function
+myFunction(a, n) 
+{
+return a[n-1];
+
+}
+
+console.log(myFunction("viraj", 2));
