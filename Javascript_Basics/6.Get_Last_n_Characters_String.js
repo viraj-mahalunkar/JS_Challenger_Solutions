c51538a9abd3db0ad92727814b9ef06f79bfe212
Viraj-Mahalunkar <mahalunkar.viraj@gmail.com>
2022-06-14 07:38:35 +0530

    completed the 6th & 7th challenge from JS_Basics

---
+++ Javascript_Basics/6.Get_Last_n_Characters_String.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+function
+myFunction(str) {
+    return str.slice(-3);
+}
+
+console.log(myFunction("Viraj"));
