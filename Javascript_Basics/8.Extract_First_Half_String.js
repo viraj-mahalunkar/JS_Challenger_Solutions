3b7f42e5f9c0427d8bc1fc2a3832bd04dd703b45
Viraj-Mahalunkar <mahalunkar.viraj@gmail.com>
2022-06-19 23:25:52 +0530

    completed JS_Basics 8th challenge

---
+++ Javascript_Basics/8.Extract_First_Half_String.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+function myFunction(a) {
+
+return console.log(a.slice(0, a.length/2));
+}
+
+myFunction("JavaScript");
+myFunction("Viraj");
